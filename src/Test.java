ab8857f417fc2cc539bfbc114a1e1caecebde4bc
BT_4N2_17 <zwu2542@bths.edu>
2023-05-03 12:34:06 -0400

    e

--- src/Test.java
+++ src/Test.java
@@ -7,6 +7,24 @@ public class Test {
     Chair c = new Chair(3,4,5);
     Player1 p1 = new Player1();
     Player2 p2 = new Player2();
+    public static void main(String[] args) {
+        Test t = new Test();
+        t.assignChair();
+        Chair c = new Chair(3,4,5);
+        if(t.getP1().getC()instanceof OfficeChair){
+            OfficeChair o = new OfficeChair(4,4,7);
+            o.grantBuff();
+        } else if (t.getP1().getC()instanceof Stools) {
+            Stools s = new Stools(3,3,3);
+            s.isDoubleHit();
+        } else if (t.getP1().getC()instanceof WoodenChair) {
+            WoodenChair w = new WoodenChair(4,4,7);
+            w.grantDebuff();
+        } else if (t.getP1().getC()instanceof Benches) {
+            Benches b = new Benches(5,4,12);
+            b.isStun();
+        }
+    }
 
     public Player1 getP1() {
         return p1;
